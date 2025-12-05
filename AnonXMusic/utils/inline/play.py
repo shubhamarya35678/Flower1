3e21703b3fd91bf3057f3aaf190f547df8f47ae2
pritamarya2627-gif <pritamarya2627@gmail.com>
2025-12-05 08:33:18 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -55,14 +55,14 @@ def stream_markup_timer(_, chat_id, played, dur):
             )
         ],
         [
-            InlineKeyboardButton(text="◁ 20s", callback_data=f"seekback {chat_id} 20"),
-            #InlineKeyboardButton(text="10s", callback_data=f"seekback {chat_id} 10"),
-            InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="◁ 20s", callback_data=f"seekback {chat_id} 20"),            #InlineKeyboardButton(text="10s", callback_data=f"seekback {chat_id} 10"),
+                        InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             #InlineKeyboardButton(text="10s", callback_data=f"seek {chat_id} 10"),
             InlineKeyboardButton(text="20s ▷", callback_data=f"seek {chat_id} 20"),
         ],
         [
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ",url=f"https://t.me/{app.username}?startgroup=true",
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+InlineKeyboardButton(text="ᴀᴅᴅ ɪᴛ",url=f"https://t.me/{app.username}?startgroup=true",
             ),
         ],
 
