1e2c9efdf28e7d3cb5dcb991b9f65172f823db12
pritamarya2627-gif <pritamarya2627@gmail.com>
2025-12-07 00:35:22 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -34,7 +34,8 @@ def private_panel(_):
         [
 InlineKeyboardButton(text="Updates", url=f"https://t.me/TgMusicBots"),
                 ], 
-              [InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
+                [
+InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG"),
         ],
         
     ]
