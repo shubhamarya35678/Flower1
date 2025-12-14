cb06a73ea0a47a7fbe7b2561242ea9cd9247bb09
pritamarya2627-gif <pritamarya2627@gmail.com>
2025-12-14 12:34:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -93,8 +93,8 @@ START_IMG_URL = getenv(
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/y1ulqf.jpg"
 )
-PLAYLIST_IMG_URL = "https://files.catbox.moe/7dzhx6.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/7dzhx6.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/tqjfpk.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/sz5wsh.jpg"
 TELEGRAM_AUDIO_URL = "https://files.catbox.moe/8a149o.jpg"
 TELEGRAM_VIDEO_URL = "https://files.catbox.moe/g25ujr.jpg"
 STREAM_IMG_URL = "https://files.catbox.moe/rp792b.jpg"
